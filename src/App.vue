31b89a81cf236e29241fca836d0fc77b086e4184
Elise Rousseau <rousseaujumelle@gmail.com>
2020-09-13 21:14:23 +0200

    mise en place d'un carousel

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <todos></todos>
+
+   <carousel>
+      <carousel-slide>
+        <img src="https://picsum.photos/id/1010/630/300/" alt="" width="100%">
+      </carousel-slide>
+      <carousel-slide>
+        <img src="https://picsum.photos/id/1010/630/300" alt="" width="100%">
+      </carousel-slide>
+
+    </carousel>
+
+      <todos></todos>
+    
   </div>
 </template>
  
@@ -8,12 +20,19 @@
 <script>
 
 import Todos from './components/Todos'
+import Carousel from './components/carousel/Carousel'
+import CarouselSlide from './components/carousel/CarouselSlide'
+
+
 
 export default {
 
   components:{
-    Todos
+    Todos, 
+    Carousel,
+    CarouselSlide
     
     },
 }
 </script>
+
---
+++ src/components/carousel/Carousel.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+        <slot></slot>
+        <button class="carousel__nav carousel__next" @click.prevent="next"><i class="fas fa-arrow-circle-right fa-3x"></i></button>
+        <button class="carousel__nav carousel__prev" @click.prevent="prev"><i class="fas fa-arrow-circle-left fa-3x"></i></button>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            index: 0,
+            slides: []
+        }
+    },
+
+    mounted(){
+        this.slides = this.$children
+        this.slides.forEach((slide,  i)=>{
+            slide.index = i
+        })
+    },
+
+    computed:{
+        slidesCount(){ return this.slides.length }
+    },
+
+    methods: {
+
+        next(){
+            this.index++
+            if(this.index >= this.slidesCount){
+                this.index = 0
+            }
+        },
+
+        prev (){
+            this.index--
+            if(this.index < 0){
+                this.index = this.slidesCount - 1
+            }
+        }
+    }
+    
+}
+</script>
+
+
+
+<style >
+
+.carousel{
+  position: relative;
+}
+
+.carousel__nav{
+  position: absolute;
+ top:150px;
+
+
+}
+.carousel__prev{
+    position: absolute;
+   left: 25px;
+}
+
+.carousel__next{
+    position: absolute;
+    right: 25px;
+}
+
+</style>
